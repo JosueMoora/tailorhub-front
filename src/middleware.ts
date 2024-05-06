5254a739b03f2c60a361a32fe91ef309952f9d8d
JosueMoora <josuedavidmora99@gmail.com>
2024-05-06 09:36:52 -0400

    log

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server'
 import { jwtVerify } from 'jose'
 export async function middleware (request: NextRequest) {
   const token = request.cookies.get('token')
+  console.log(token);
+  
   if (token === undefined) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
