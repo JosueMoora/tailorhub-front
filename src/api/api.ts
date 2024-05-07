5258a754bcdfbece33987f21613c3814a265b49e
JosueMoora <josuedavidmora99@gmail.com>
2024-05-06 21:30:13 -0400

    [add] url backend

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const baseURL = 'http://localhost:3001/api'
-const baseURL = 'https://tailorhub-back.vercel.app/api/'
+const baseURL = 'https://tailorhub-back.vercel.app/api'
 
 const restaurantApi = axios.create({baseURL})
 
